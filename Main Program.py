4cc794d9286772fa39687cee43e6f1c5b8cc32d3
devguarv <dev.prakash@education.nsw.gov.au>
2024-04-22 16:19:03 +1000

    Update Main Program.py

--- Main Program.py
+++ Main Program.py
@@ -1,11 +1,25 @@
 from tkinter import *
+from customtkinter import *
 
+
+#Main Window Creation
 root = Tk()
 root.title("Trifecta Quest")
 
+
 default_font_name = "Arial"
-defaulty_font_size = 9
+default_font_size = 9
 default_geometry_x = 500
 default_geometry_y = 500 
 
+
 root.geometry("{width}x{height}".format(width=default_geometry_x, height=default_geometry_y))
+
+
+
+
+
+
+
+
+root.mainloop()
