b87799537dd6900f20717ecc7ab6eee8cb34ad0b
devguarv <dev.prakash@education.nsw.gov.au>
2024-04-22 16:26:18 +1000

    Installed CTK, created button to begin
    Created Begin btn and placed it using center and relx and rely

--- Main Program.py
+++ Main Program.py
@@ -3,7 +3,7 @@ from customtkinter import *
 
 
 #Main Window Creation
-root = Tk()
+root = CTk()
 root.title("Trifecta Quest")
 
 
@@ -15,6 +15,9 @@ default_geometry_y = 500
 
 root.geometry("{width}x{height}".format(width=default_geometry_x, height=default_geometry_y))
 
+#Creating the begin button
+begin_btn = CTkButton(master=root, text="Begin")
+begin_btn.place(relx = 0.5, rely=0.5, anchor="center")
 
 
 
