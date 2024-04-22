1cdc440246a0cdba017c5675ec90090f09838b02
devguarv <dev.prakash@education.nsw.gov.au>
2024-04-22 16:03:31 +1000

    Basic Structure

--- Main Program.py
+++ Main Program.py
@@ -1,3 +1,11 @@
 from tkinter import *
 
 root = Tk()
+root.title("Trifecta Quest")
+
+default_font_name = "Arial"
+defaulty_font_size = 9
+default_geometry_x = 500
+default_geometry_y = 500 
+
+root.geometry("{width}x{height}".format(width=default_geometry_x, height=default_geometry_y))
