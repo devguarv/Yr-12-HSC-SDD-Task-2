5465138be6ece52de916c3d7c3105c7ca6b18cb2
devguarv <dev.prakash@education.nsw.gov.au>
2024-04-23 19:23:19 +1000

    Added CTK button icon
    Added a size change to the image / icon added to the CTk Button and changed text colour of button to white and assigned the image icon to the right of the text

--- Main Program.py
+++ Main Program.py
@@ -7,23 +7,28 @@ root = CTk()
 root.title("Trifecta Quest")
 
 
-default_font_name = "Arial"
-default_font_size = 9
 default_geometry_x = 500
-default_geometry_y = 500 
+default_geometry_y = 500
 
 set_appearance_mode("light")
 
-arrow_img_path = "C:\\Users\\devth\\OneDrive\\Desktop\\Assignment\\Yr-12-HSC-SDD-Task-2\\right-arrow.png"
+arrow_img_path = "C:\\Users\\devth\\OneDrive\\Desktop\\Assignment\\Yr-12-HSC-SDD-Task-2\\arrow-24-24.png"
 arrow_img = Image.open(arrow_img_path)
-arrow_img = ImageTk.PhotoImage(arrow_img)
-resize_arrow_img = arrow_img.resize((100,100))
+
+# Define the desired smaller size
+small_arrow_size = (20, 20)
+
+# Resize the arrow image
+resize_arrow_img = arrow_img.resize(small_arrow_size)
+
+# Convert resized image to PhotoImage
+img = ImageTk.PhotoImage(resize_arrow_img)
 
 root.geometry("{width}x{height}".format(width=default_geometry_x, height=default_geometry_y))
 
-#Creating the begin button
-begin_btn = CTkButton(master=root, text="Begin", image=arrow_img)
-begin_btn.place(relx = 0.5, rely=0.5, anchor="center")
+# Creating the begin button
+begin_btn = CTkButton(master=root, text="Begin", text_color="White", image=img, compound="right")
+begin_btn.place(relx=0.5, rely=0.5, anchor="center")
 
 
 
